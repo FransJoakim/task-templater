28219ad3c0ad2048a4631e4ff12ae15982a89fd0
Frans Joakim Titulaer <fransjlt@gmail.com>
2024-01-08 19:53:16 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,65 @@
 import React from "react";
 import "./App.css";
 import data from "./data.json";
+import { atomFamily, useRecoilState } from "recoil";
 
 type Attributes = { [key: string]: string | undefined };
 
 type NodeInterface = {
+  id: string;
   nodeName: string;
   attributes?: Attributes;
-  children?: (string | NodeInterface)[];
+  children?: (NodeInterface | string)[];
 };
 
-const createChildNodes = (nodes: (string | NodeInterface)[]) => {
-  return nodes.map((node, i) => {
-    if (typeof node === "object") {
-      return <Node key={i} node={node} />;
-    } else {
-      return node;
-    }
-  });
-};
+const NodeAtom = atomFamily<NodeInterface | null, string>({
+  key: "node",
+  default: null,
+});
 
-const Node = ({ node }: { node: NodeInterface }) => {
-  const children = node.children ? createChildNodes(node.children) : null;
-  return React.createElement(node.nodeName, node.attributes ?? null, children);
+const Node = ({ data }: { data: NodeInterface }) => {
+  // this whole motherfucker needs to be a object, with a method for rendering the component
+  // the values (i.e. string value) needs to be accessible (as JSON) and part of a state that could be updated
+  // this motherfucker also needs to be movable
+
+  const children = !data.children
+    ? null
+    : data.children.map((child, i) => {
+        if (typeof child === "object") {
+          return <Node key={i} data={child} />;
+        } else {
+          return child;
+        }
+      });
+  const Component = (
+    <span style={{ display: "flex" }}>
+      {React.createElement(
+        data.nodeName,
+        {
+          ...data.attributes,
+          onClick: (e: React.MouseEvent<Element, MouseEvent>) => {
+            e.stopPropagation();
+            console.log("clicked", data.nodeName);
+          },
+        },
+        children
+      )}
+      <button>click</button>
+    </span>
+  );
+
+  const [node, setNode] = useRecoilState(NodeAtom(data.id));
+  if (!node) {
+    setNode({
+      ...data,
+      children: data.children?.map((child) => {
+        // TODO: This is a hack to get around the fact that strings are not valid nodes
+        if (typeof child === "string") return "";
+        return child.id;
+      }),
+    });
+    return null;
+  }
 };
 
 function App() {
@@ -30,7 +67,7 @@ function App() {
     <main>
       <div className="container">
         <form>
-          <Node node={data} />
+          <Node data={data} />
           <input type="submit" value="Submit" />
         </form>
       </div>
