f6d341e79f63687b0afc2f5a88fc0e4b27e0685c
Frans Joakim Titulaer <fransjlt@gmail.com>
2024-01-15 12:01:48 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
 import data from "./data.json";
-import { atomFamily, useRecoilState, useSetRecoilState } from "recoil";
+import {
+  atomFamily,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
 
 type Attributes = { [key: string]: string | undefined };
 
@@ -19,50 +24,62 @@ const NodeAtom = atomFamily<NodeInterface | null, string>({
   default: null,
 });
 
-const InitAtoms = (data: NodeInterface, parent: string | null) => {
-  const setNode = useSetRecoilState(NodeAtom(data.id));
-  setNode({
-    ...data,
-    parent,
-    children: data.children?.map((child) => {
-      if (child.nodeName === "#text") return child;
-      return {
-        id: child.id,
-        nodeName: child.nodeName,
-      };
-    }),
-  });
-  data.children?.forEach((child) => {
-    if (child.nodeName === "#text") return;
-    InitAtoms(child, data.id);
-  });
+interface MountProps {
+  data: NodeInterface;
+  parent: string | null;
+}
+
+const Mount = ({ data, parent }: MountProps) => {
+  console.log(data.nodeName, data.id, parent);
+  const [node, setNode] = useRecoilState(NodeAtom(data.id));
+
+  useEffect(() => {
+    if (!node) {
+      setNode({
+        ...data,
+        parent,
+        children: data.children?.map((child) => {
+          if (child.nodeName === "#text") return child;
+          return {
+            id: child.id,
+            nodeName: child.nodeName,
+          };
+        }),
+      });
+    }
+  }, [data, node, parent, setNode]);
+
+  return (
+    <>
+      {data.children?.forEach((child) => {
+        return <Mount data={child} parent={data.id} />;
+      })}
+    </>
+  );
 };
 
 interface NodeProps {
-  data: NodeInterface;
+  id: string;
 }
 
-const Node = ({ data }: NodeProps) => {
-  const [node, setNode] = useRecoilState(NodeAtom(data.id));
+const Node = ({ id }: NodeProps) => {
+  const [node, setNode] = useRecoilState(NodeAtom(id));
   if (!node) return null;
 
-  // rewrite: only gets data from state
-  const children = !data.children
-    ? null
-    : data.children.map((child, i) => {
-        if (typeof child === "object") {
-          return <Node key={i} data={child} />;
-        } else {
-          return child;
-        }
-      });
+  const children = node.children?.map((child, i) => {
+    if (child.nodeName === "#text") {
+      return child.textContent;
+    } else {
+      return <Node key={i} id={child.id} />;
+    }
+  });
   const Component = (
     <span style={{ display: "flex" }}>
-      {React.createElement(data.nodeName, {
-        ...data.attributes,
+      {React.createElement(node.nodeName, {
+        ...node.attributes,
         onClick: (e: React.MouseEvent<Element, MouseEvent>) => {
           e.stopPropagation();
-          console.log("clicked", data.nodeName);
+          console.log("clicked", node.nodeName);
         },
         children,
       })}
@@ -78,15 +95,15 @@ const Node = ({ data }: NodeProps) => {
 };
 
 function App() {
-  React.useEffect(() => {
-    InitAtoms(data, null);
-  }, []);
+  const init = useRef(false);
+
+  if (!init.current) return <Mount data={data} parent="form" />;
 
   return (
     <main>
       <div className="container">
         <form>
-          <Node data={data} />
+          <Node id={data.id} />
           <input type="submit" value="Submit" />
         </form>
       </div>
